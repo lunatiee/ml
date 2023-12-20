92c2a41fa09ce93043d4aeefbd339839eab8cf79
lunatiee <lunatie@gmail.com>
2023-12-20 19:49:10 +0900

    for문

--- w2v1.py
+++ w2v1.py
@@ -4,6 +4,7 @@ import zipfile
 from lxml import etree
 from nltk.tokenize import word_tokenize, sent_tokenize
 
+
 # 데이터 다운로드
 urllib.request.urlretrieve("https://raw.githubusercontent.com/ukairia777/tensorflow-nlp-tutorial/main/09.%20Word%20Embedding/dataset/ted_en-20160408.xml", filename="ted_en-20160408.xml")
 
